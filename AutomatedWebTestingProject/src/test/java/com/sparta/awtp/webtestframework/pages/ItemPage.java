0e1bcf166b67049f0264a6da8183590c2673b53c
Chris Rayner <crayner@spartaglobal.com>
2024-08-29 16:09:09 +0100

    added methods to CheckoutPage for checking number of items, and item Page for adding item to cart@

--- AutomatedWebTestingProject/src/test/java/com/sparta/awtp/webtestframework/pages/ItemPage.java
+++ AutomatedWebTestingProject/src/test/java/com/sparta/awtp/webtestframework/pages/ItemPage.java
@@ -1,4 +1,19 @@
 package com.sparta.awtp.webtestframework.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ItemPage {
+
+    private WebDriver driver;
+    private By addToCart = new By.ByClassName("add-to-cart");
+
+    public ItemPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickAddToCart() {
+        driver.findElement(addToCart).click();
+    }
+
 }
--- AutomatedWebTestingProject/src/test/java/com/sparta/awtp/webtestframework/pages/ViewCartPage.java
+++ AutomatedWebTestingProject/src/test/java/com/sparta/awtp/webtestframework/pages/ViewCartPage.java
@@ -1,4 +1,36 @@
 package com.sparta.awtp.webtestframework.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class ViewCartPage {
+
+    private WebDriver driver;
+    private By CartItemTable = new By.ById("cart_info_table");
+    private By checkoutButton = new By.ByClassName("check_out");
+    private By removeItemButton = new By.ByClassName("cart_delete");
+
+
+    public ViewCartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public List<WebElement> getCartItems(){
+        return driver.findElements(CartItemTable);
+    }
+    public void clickCheckoutButton(){
+        driver.findElement(checkoutButton).click();
+    }
+    public void clickRemoveItemButton(){
+        driver.findElement(removeItemButton).click();
+    }
+    public By getCartItem(String itemId){
+        return By.id(itemId);
+    }
+    public String getItemDescription(String itemId){
+        return driver.findElement(getCartItem(itemId)).getAttribute("cart_description");
+    }
+
 }
