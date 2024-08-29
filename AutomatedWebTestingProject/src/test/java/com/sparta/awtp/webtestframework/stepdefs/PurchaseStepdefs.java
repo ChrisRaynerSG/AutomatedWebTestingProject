3783353db8965fc7184f0e2627f58e3ade51f683
Chris Rayner <crayner@spartaglobal.com>
2024-08-29 14:31:26 +0100

    init PurchaseStepDefs

---
+++ AutomatedWebTestingProject/src/test/java/com/sparta/awtp/webtestframework/stepdefs/PurchaseStepdefs.java
@@ -0,0 +1,124 @@
+package com.sparta.awtp.webtestframework.stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PurchaseStepdefs {
+    @When("I click add to cart")
+    public void iClickAddToCart() {
+    }
+
+    @And("the item is in stock")
+    public void theItemIsInStock() {
+    }
+
+    @Then("I should see my cart updated")
+    public void iShouldSeeMyCartUpdated() {
+    }
+
+    @Given("I am on the products page")
+    public void iAmOnTheProductsPage() {
+    }
+
+    @Given("I am on a specific items page")
+    public void iAmOnASpecificItemsPage() {
+    }
+
+    @Given("I am on the view_cart page")
+    public void iAmOnTheView_cartPage() {
+    }
+
+    @And("I have an item in my cart")
+    public void iHaveAnItemInMyCart() {
+    }
+
+    @And("I am not logged in")
+    public void iAmNotLoggedIn() {
+    }
+
+    @When("I click Proceed to checkout")
+    public void iClickProceedToCheckout() {
+    }
+
+    @Then("I should be instructed to login or register")
+    public void iShouldBeInstructedToLoginOrRegister() {
+    }
+
+    @And("I am logged in")
+    public void iAmLoggedIn() {
+    }
+
+    @Then("I should be directed to the checkout page")
+    public void iShouldBeDirectedToTheCheckoutPage() {
+    }
+
+    @And("my address details should be correct")
+    public void myAddressDetailsShouldBeCorrect() {
+    }
+
+    @Given("I am on the checkout page")
+    public void iAmOnTheCheckoutPage() {
+    }
+
+    @When("I click place order")
+    public void iClickPlaceOrder() {
+    }
+
+    @Then("I should be directed to the payment page")
+    public void iShouldBeDirectedToThePaymentPage() {
+    }
+
+    @Given("I am on the payment page")
+    public void iAmOnThePaymentPage() {
+    }
+
+    @And("I have not input my card details")
+    public void iHaveNotInputMyCardDetails() {
+    }
+
+    @When("I click pay and confirm order")
+    public void iClickPayAndConfirmOrder() {
+    }
+
+    @Then("I should be informed what is missing")
+    public void iShouldBeInformedWhatIsMissing() {
+    }
+
+    @And("remain on the payment page")
+    public void remainOnThePaymentPage() {
+    }
+
+    @And("I have input my card details")
+    public void iHaveInputMyCardDetails() {
+    }
+
+    @Then("I should be directed to the payment_done page")
+    public void iShouldBeDirectedToThePayment_donePage() {
+    }
+
+    @And("be informed with the message {string}")
+    public void beInformedWithTheMessage(String arg0) {
+    }
+
+    @And("I have items in my cart")
+    public void iHaveItemsInMyCart() {
+    }
+
+    @Then("the correct number of items should be displayed")
+    public void theCorrectNumberOfItemsShouldBeDisplayed() {
+    }
+
+    @And("the correct items should be displayed")
+    public void theCorrectItemsShouldBeDisplayed() {
+    }
+
+    @When("I click remove item")
+    public void iClickRemoveItem() {
+    }
+
+    @Then("the item should be removed from my cart")
+    public void theItemShouldBeRemovedFromMyCart() {
+    }
+}
